e43865d7c3df3161e120505c31ce907c203358c6
des <sachedes@students.zhaw.ch>
2015-03-01 22:10:31 +0100

    added more comments

--- F02_H_printLess/main.c
+++ F02_H_printLess/main.c
@@ -31,7 +31,6 @@ int fdout;
  * http://www.linuxquestions.org/questions/programming-9/get-width-height-of-a-terminal-window-in-c-810739/
  * 
  */
-
 void getConsoleWindowSize(int* pcols, int* plines) {
     #ifdef TIOCGSIZE
         struct ttysize ts;
@@ -46,6 +45,10 @@ void getConsoleWindowSize(int* pcols, int* plines) {
     #endif /* TIOCGSIZE */
 }
 
+/*
+ * read line by receiving the fd value, current position to read, the pointer to the buffer and 
+ * the size that should be read
+ */
 int readLine(int fdout, int pos, char* pOutBuffer, int size) {    
     int charsRead = 0;
     int lineSize = 0;
@@ -54,6 +57,10 @@ int readLine(int fdout, int pos, char* pOutBuffer, int size) {
     lseek(fdout, pos, SEEK_SET);
     charsRead = read(fdout, pOutBuffer, size);
     
+    /*
+     * check if the next line read contains a "\n" and if yes, remember the location and 
+     * give this information back
+     */
     for (i = 0; i < charsRead; i++)
     {
         if (pOutBuffer[i] == '\n')
@@ -67,8 +74,12 @@ int readLine(int fdout, int pos, char* pOutBuffer, int size) {
     return lineSize;
 }
 
-//http://ubuntuforums.org/showthread.php?t=225713&page=2&s=9360ceae8150887a6cf1cb3497926c8c
 
+/*
+ * Source from: 
+ * http://ubuntuforums.org/showthread.php?t=225713&page=2&s=9360ceae8150887a6cf1cb3497926c8c
+ * reads 1 character from input without displaying it
+ */
 int getch(void)
 {
     int ch;
@@ -82,7 +93,9 @@ int getch(void)
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt); /*reapply the old settings */
     return ch; /*return received char */
 }
-
+/*
+ * Main Function, started with 1 argument (the file that should be displayed)
+ */
 int main(int argc, char** argv) {
     
     int cols = 80;
@@ -94,6 +107,9 @@ int main(int argc, char** argv) {
     _Bool quit;
     _Bool validInput;
     
+    /*
+     * error when no argument delivered or too many are given
+     */
     if(argv[2] != NULL || argv[1] == NULL) {
         printf("Program requires 1 argument (filename) \n");
         return -1;
@@ -101,30 +117,54 @@ int main(int argc, char** argv) {
     else {
         getConsoleWindowSize(&cols, &lines);
     
+        /* allocate needed space for one line
+         */
         pLine = malloc(cols + 1);
     
-        const char *FILENAME = argv[1]; //"/home/des/git/betriebssystem-ha/F02_H_printLess/itskylib.h"; 
+        /* work with file name that is argv[1]
+         */
+        const char *FILENAME = argv[1]; 
     
         fdout = open(FILENAME, O_RDONLY, 0777);
         pos = 0;
         quit = 0;
         validInput = 1;
         
+        /*
+         * run program while not quitted by user
+         */
         while (quit == 0)
         {
+            /* 
+             * only read next line if valid input was received
+             */
             if (validInput)
             {
+                /* return the line size 
+                 */
                 charsRead = readLine(fdout, pos, pLine, cols);
         
-                //printf("No: %d\n", charsRead);
-
+                /* insert new line character at the end of the line
+                 */
                 pLine[charsRead] = '\0';  
 
+                /* print line
+                 */
                 printf("%s", pLine);
+                //TO-DO: print complete window size - 
+                /* - for loop for print out  as many lines there are
+                 * - fill each charsRead value into an array to remember when jumping back
+                 *    challenge: defining array size for these charsRead as only complete when full file is read
+                 * 
+                 */
             }
             
+            /* read character for input (see method getch())
+             */
             key = getch();
-            
+            /*
+             * menu that selects action for input-key
+             */
             switch (key)
             {
                 case 'k':
@@ -135,6 +175,14 @@ int main(int argc, char** argv) {
                     quit = 1;
                     validInput = 1;
                     break;
+                //TO-DO: implement more cases like j, f, g  
+                    /*
+                     * j: previous line: print out full window but from line before
+                     * f: jump to beginning pos=0
+                     * g: jump to end (needs to be calculated first)
+                     * other options like one page down uses window size, rest from calculation point of view is 
+                     * same as line up or down
+                     */
                 default:
                     validInput = 0;
                     break;
