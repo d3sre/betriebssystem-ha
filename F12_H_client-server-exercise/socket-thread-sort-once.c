a95de2193191ed57b97e215f80a72b1fc14f7d7f
des <sachedes@students.zhaw.ch>
2015-05-31 22:38:10 +0200

    added usage and arguments from threading until line nr 119

--- F12_H_client-server-exercise/socket-thread-sort-once.c
+++ F12_H_client-server-exercise/socket-thread-sort-once.c
@@ -21,7 +21,43 @@
 #include <sys/types.h>
 #include <unistd.h>     /* for close() */
 
+#include <pthread.h>
+#include <stdbool.h>
+
 #include <itskylib.h>
+#include <hsort.h>
+#include <fsort.h>
+#include <isort.h>
+#include <ternary-hsort.h>
+#include <msort.h>
+#include <fsort-metrics.h>
+#include <psort.h>
+
+const int LINE_SIZE = 200;
+const int THREAD_COUNT = 20;
+
+pthread_once_t once = PTHREAD_ONCE_INIT;
+
+enum mt_sort_type { MT_HEAP_SORT, MT_TERNARY_HEAP_SORT, MT_QUICK_SORT, MT_FLASH_SORT, MT_INSERTION_SORT, MT_MERGE_SORT };
+
+const char *file_name;
+enum mt_sort_type selected_sort_type;
+struct string_array content;
+
+
+struct thread_arg {
+    int thread_idx;
+    pthread_once_t *once;
+};
+
+/* needs to be global here for the sake of thread_once */
+struct thread_arg *thread_data;
+
+void *thread_run(void *ptr);
+
+void print_sorted_words(struct thread_arg *arg);
+
+void thread_once();
 
 #define RCVBUFSIZE 32   /* Size of receive buffer */
 #define MAXPENDING 5    /* Maximum outstanding connection requests */
@@ -31,7 +67,19 @@ void usage(const char *argv0, const char *msg) {
         printf("%s\n\n", msg);
     }
     printf("Usage\n\n");
-    printf("%s <Server Port>\n", argv0);
+    printf("splits the given input file into words, disregarding all characters other than [A-Za-z0-9\240-\377]\n");
+    printf("(non-used characters mark word boundaries)\n");
+    printf("sorts the words and prints out the sorted words in a 200 character wide output\n");
+    printf("words longer than 200 characters exceed the width in a line by themselves\n");
+    printf("this is done 20 times in different threads started at random times 0..9 sec delayed after program start\n");
+    printf("reading and sorting is only done once by the thread that comes first, output is then rendered and printed by each thread separately\n\n");
+
+    printf("%s <Server Port> -f file \n\tsorts contents of file using flashsort.\n\n", argv0);
+    printf("%s <Server Port> -h file \n\tsorts contents of file using heapsort.\n\n", argv0);
+    printf("%s <Server Port> -t file \n\tsorts contents of file using ternary heapsort.\n\n", argv0);
+    printf("%s <Server Port> -q file \n\tsorts contents of file using quicksort.\n\n", argv0);
+    printf("%s <Server Port> -i file \n\tsorts contents of file using insertionsort.\n\n", argv0);
+    printf("%s <Server Port> -m file \n\tsorts contents of file using mergesort.\n\n", argv0);
     exit(1);
 }
 
@@ -40,7 +88,9 @@ void handle_tcp_client(int client_socket);   /* TCP client handling function */
 int main(int argc, char *argv[]) {
 
     int retcode;
+    pthread_t *thread;
 
+    char *argv0 = argv[0];
     if (is_help_requested(argc, argv)) {
         usage(argv[0], "");
     }
@@ -52,11 +102,45 @@ int main(int argc, char *argv[]) {
     unsigned short server_port;     /* Server port */
     unsigned int client_address_len;            /* Length of client address data structure */
 
-    if (argc != 2) {
+    if (argc < 4) {
         /* Test for correct number of arguments */
         usage(argv[0], "wrong number of arguments");
     }
     server_port = atoi(argv[1]);  /* First arg:  local port */
+
+    int opt_idx = 2;
+
+    /* TODO consider using getopt instead!! */
+    char *argv_opt = argv[opt_idx];
+    if (strlen(argv_opt) < 4|| argv_opt[0] != '-') {
+        usage(argv0, "wrong option");
+    }
+    char opt_char = argv_opt[2];
+    switch (opt_char) {
+        case 'h' :
+            selected_sort_type = MT_HEAP_SORT;
+            break;
+        case 't' :
+            selected_sort_type = MT_TERNARY_HEAP_SORT;
+            break;
+        case 'f' :
+            selected_sort_type = MT_FLASH_SORT;
+            break;
+        case 'q' :
+            selected_sort_type = MT_QUICK_SORT;
+            break;
+        case 'i' :
+            selected_sort_type = MT_INSERTION_SORT;
+            break;
+        case 'm' :
+            selected_sort_type = MT_MERGE_SORT;
+            break;
+        default:
+            usage(argv0, "wrong option");
+            break;
+    }
+
+    file_name = argv[3];
 
     /* Create socket for incoming connections */
     server_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
